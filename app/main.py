ded487119d55d11568b2ef07831838783020a6bd
Niroop23 <shanmukhaniroop19@gmail.com>
2025-12-30 17:36:04 +0530

    added invite routers to main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from app.routes import authRoutes, org_routes
+from app.routes import authRoutes, org_routes,inviteRoutes
 
 app=FastAPI()
 
 
 app.include_router(authRoutes.router)
 app.include_router(org_routes.router)
+app.include_router(inviteRoutes.router)
 
 @app.get("/")
 def root():
