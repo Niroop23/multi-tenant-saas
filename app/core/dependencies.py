51c836d2485524796bfcc9e712e0e5d7ec932492
Niroop23 <shanmukhaniroop19@gmail.com>
2026-01-01 14:29:45 +0530

    refactor(auth): resolve membership directly from access token when possible

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -6,14 +6,13 @@ from app.database.session import get_db
 from app.models.membership import Membership
 from app.models.user import User
 from app.schemas.token import TokenData
-
+from app.core.security import bearer_scheme
 
 def get_current_membership(
     org_id: UUID | None = None,
     org_header_id: UUID | None = Header(None, alias="X-Org-Id"),
-    current_user: User = Depends(get_current_user),
-    token_data:TokenData=Depends(verify_access_token),
     db: Session = Depends(get_db),
+    credentials= Depends(bearer_scheme)
 ):
     # If both are provided, they MUST match
     if org_id and org_header_id and org_id != org_header_id:
@@ -21,26 +20,25 @@ def get_current_membership(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Org ID mismatch between path and X-Org-Id header",
         )
+        
+    creds_exception=HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="couldn't validate credentials")
     
-    if token_data.org_id:
-        return Membership(
-            user_id=current_user.id,
-            org_id=token_data.org_id,
-            role=token_data.role,
-        )
+    token_data=verify_access_token(credentials.credentials,creds_exception)
 
     effective_org_id = org_id or org_header_id
 
     if not effective_org_id:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Organization id is missing (path param or X-Org-Id header)",
+            detail="Organization id is missing ",
         )
+    if token_data.org_id== effective_org_id and token_data.role:
+        return token_data
 
     membership = (
         db.query(Membership)
         .filter(
-            Membership.user_id == current_user.id,
+            Membership.user_id == token_data.id,
             Membership.org_id == effective_org_id,
         )
         .first()
